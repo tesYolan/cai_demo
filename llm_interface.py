961abcb73c6f0555049365ef2540932ab5d57eb0
Tesfa Yohannes <tmyohannes@gmail.com>
2023-10-17 16:27:24 +0800

    llm_interface fix

--- llm_interface.py
+++ llm_interface.py
@@ -144,4 +144,4 @@ async def predict(prompt: dict):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=6888)
+    uvicorn.run(app, host="0.0.0.0", port=8516)
